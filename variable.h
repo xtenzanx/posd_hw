5d471e8bfb49e445bbe38eb6ac3fabc77c334130
Tenzan <xtenzanxt1@gmail.com>
2017-10-18 23:49:03 +0800

    test

--- variable.h
+++ variable.h
@@ -9,7 +9,8 @@ class Atom;
 class Number;
 class Struct;
 
-class Variable : public Term{
+class Variable : public Term
+{
 public:
   Variable(string s):_symbol(s),_value(s){}
   string const _symbol;
